3d8d5df0b1806c3221e26e4cddf4ed30ea2aabcf
Terekhov-A-S <Kotto@student.su>
2026-05-04 20:03:14 +0300

    modify main

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("=== Калькулятор ===")
+	fmt.Print("Введите имя пользователя: ")
+	username, _ := reader.ReadString('\n')
+	username = strings.TrimSpace(username)
+
+	// Здесь будем проверять пользователя
+	fmt.Printf("Привет, %s!\n", username)
 }
